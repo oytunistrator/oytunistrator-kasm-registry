2bce33fc3cbecd81f55e5780355615dae0b6637c
Oytunistrator <3594944+oytunistrator@users.noreply.github.com>
2024-12-09 21:12:57 +0300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Oytunistrator Repository',
+    description: 'Oytunistrator Repository for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/oytunistrator/oytunistrator-kasm-registry/',
+    contactUrl: 'https://github.com/oytunistrator/oytunistrator-kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
